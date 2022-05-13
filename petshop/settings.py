c9f7453925fb04b470e155590bfc205688b1dbd7
Breno Silva <brenokcc@yahoo.com.br>
2022-05-13 20:37:48 -0300

    Continuação da implementação

--- petshop/settings.py
+++ petshop/settings.py
@@ -66,7 +66,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'sloth.core.views.context_processor'
             ],
         },
     },
@@ -167,5 +166,8 @@ SLOTH = {
 	'INCLUDE': {
 		'CSS': [],
 		'JS': [],
+	},
+	'ROLES':{
+		'ALLOW_MULTIPLE': True
 	}
 }
